d869eec3b1bf792092c484205875a2808c892619
MaedehBDNF <badanfirouzm@gmail.com>
2023-03-15 15:05:58 +0330

    complete Movie class - inherits abstract class TVShow - add new attribute - add needed methods

--- src/main/java/org/example/Movie.java
+++ src/main/java/org/example/Movie.java
@@ -2,12 +2,33 @@ package org.example;
 
 import java.util.ArrayList;
 
-class Movie extends TVShow {
-    /*
-     *Movie is extended from TVShow and has extra attribute length.
-     */
-    public Movie()
-    {
-        super();
+public class Movie extends TVShow {
+    private int lengthInMinutes;
+
+    public Movie(String title, String genre, int yearOfPublication, int lengthInMinutes,String generateCountry, ArrayList<String> casts) {
+        super(title, genre, yearOfPublication,generateCountry, casts);
+        this.lengthInMinutes = lengthInMinutes;
+    }
+
+    public Movie(Movie movie) {
+        super(movie.getTitle(), movie.getGenre(), movie.getYearOfPublication(), movie.getGenerateCountry(), movie.getCastList());
+        this.lengthInMinutes = movie.getLengthInMinutes();
+    }
+
+    public int getLengthInMinutes() {
+        return lengthInMinutes;
+    }
+
+    public void setLengthInMinutes(int lengthInMinutes) {
+        this.lengthInMinutes = lengthInMinutes;
+    }
+
+    @Override
+    public String toString() {
+        int hour = lengthInMinutes / 60;
+        int minute = lengthInMinutes - (60 * hour);
+        return super.toString() + "\n" +
+                "   Length: " + ((hour > 0) ? (hour + "h ") : "") + minute + "m" + "\n";
     }
 }
+
